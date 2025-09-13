ea8159ab1a8a9f602e1b07888b54d6d7da8c5dbc
mehrab khanmohammadi <programmer.py.mail@gmail.com>
2025-09-14 00:15:00 +0330

    create def list_dir in utils file and run in main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-ECHO is on.
+from utils import list_dir
+
+
+if __name__ == "__main__" :
+    show_list = list_dir(path=r"C:\Users\XMART\Desktop\text")
+    if isinstance(show_list,list) :
+        for i in show_list :
+            print(i)
+    else :
+        print(show_list)
--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
-ECHO is on.
+import os
+
+def list_dir(path:str) :
+    if not os.path.exists(path) :
+        return "There is wrong path"
+    item = os.listdir(path)
+    if not item :
+        return "There is noting"
+    return item
