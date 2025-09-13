9f22adb8f5c5bd8b7236821c1ed609fb56609dba
mehrab khanmohammadi <programmer.py.mail@gmail.com>
2025-09-14 01:30:58 +0330

    create def create_folder in utils.py and test in main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from utils import list_dir
+from utils import create_folder
 
+
+# if __name__ == "__main__" :
+#     show_list = list_dir(path=r"C:\Users\XMART\Desktop\text")
+#     if isinstance(show_list,list) :
+#         for i in show_list :
+#             print(i)
+#     else :
+#         print(show_list)
 
 if __name__ == "__main__" :
-    show_list = list_dir(path=r"C:\Users\XMART\Desktop\text")
-    if isinstance(show_list,list) :
-        for i in show_list :
-            print(i)
-    else :
-        print(show_list)
+    print(create_folder(path=r"C:\Users\XMART\Desktop\text",name="esteghlal"))
--- utils.py
+++ utils.py
@@ -7,3 +7,11 @@ def list_dir(path:str) :
     if not item :
         return "There is noting"
     return item
+
+def create_folder(path:str,name) :
+    new_path = os.path.join(path,name)
+    if os.path.exists(new_path) :
+        return "Folder already exists"
+    else :
+        os.mkdir(new_path)
+        return "Create Folder"
