ec8765845d8ba35b1b76e4922725ad5bf4dbe03e
mehrab khanmohammadi <programmer.py.mail@gmail.com>
2025-09-19 01:46:27 +0330

    create def move item(file and folder) and def create file and def copy file in utils.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils import list_dir,create_folder,delete_folder
+from utils import list_dir,create_folder,delete_item,move_item
+from utils import create_file,copy_file
 import argparse
 
 parser = argparse.ArgumentParser(description="Smile File Manager")
@@ -13,6 +14,20 @@ c_folder.add_argument("name",help="Name for new Folder")
 d_folder = sub_argument.add_parser("d",help="Delete File or Folder to list")
 d_folder.add_argument("path",help="Path of the File or Folder to Delete")
 d_folder.add_argument("name",help="Name for Delete File or Folder")
+
+move = sub_argument.add_parser("move",help="for move File or Folder")
+move.add_argument("ex_path",help="ex path for moved")
+move.add_argument("new_path",help="new path for moved")
+move.add_argument("name",help="Name File or Folder for moved")
+
+cr_file = sub_argument.add_parser("cfile",help="for create file")
+cr_file.add_argument("path",help="Path of the File to Create")
+cr_file.add_argument("name",help="Name for new File")
+
+c_file = sub_argument.add_parser("copy_file",help="For copy file")
+c_file.add_argument("ex_path",help="File source address")
+c_file.add_argument("new_path",help="File destination address")
+c_file.add_argument("name",help="Name of the desired File")
 args = parser.parse_args()
 
 if args.command == "list" :
@@ -28,7 +43,17 @@ if args.command == "cf" :
     print(result)
 
 if args.command == "d" :
-    result = delete_folder(args.path,args.name)
+    result = delete_item(args.path,args.name)
     print(result)
 
+if args.command == "move" :
+    result= move_item(args.ex_path,args.new_path,args.name)
+    print(result)
 
+if args.command == "cfile" :
+    result = create_file(args.path,args.name)
+    print(result)
+
+if args.command == "copy_file" :
+    result = copy_file(args.ex_path,args.new_path,args.name)
+    print(result)
--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def create_folder(path:str,name) :
         os.mkdir(new_path)
         return "Create Folder"
 
-def delete_folder(path:str,name) :
+def delete_item(path:str,name) :
     delete = os.path.join(path,name)
     if not os.path.exists(delete) :
         return "There is noting"
@@ -28,5 +28,41 @@ def delete_folder(path:str,name) :
         elif os.path.isdir(delete) :
             shutil.rmtree(delete)
             return "Deleted your Folder"
+
+def move_item(current_path:str,new_path:str,name) :
+    ex_path = os.path.join(current_path,name)
+    if not os.path.exists(ex_path) :
+        return "The current path is incorrect"
+    if not os.path.exists(new_path) :
+        return "The new path is incorrect"
+    if os.path.isfile(ex_path) :
+        shutil.move(ex_path,new_path)
+        return "File moved to new destination"
+    if os.path.isdir(ex_path) :
+        shutil.move(ex_path,new_path)
+        return "Folder moved to new destination"
+    else :
+        return "unknown item type"
+
+def create_file(path:str,name) :
+    cr_file = os.path.join(path,name)
+    if os.path.exists(cr_file) :
+        return "Similar File exists"
+    with open(cr_file,"w") :
+        pass
+    return "Create File"
+
+def copy_file(ex_path:str,new_path:str,name) :
+    my_file = os.path.join(ex_path,name)
+    dest_file = os.path.join(new_path,name)
+    if not os.path.exists(my_file) :
+        return "File does not exists"
+    if os.path.exists(dest_file) :
+        return "Similar File exists"
+    shutil.copy2(my_file, dest_file)
+    return "File is Copy"
+
+
+
 
 
